6baaf4dd3d6cff7214bd3a1d69f058c95c410be1
Alexander Stoichkov <sashostoichkov@gmail.com>
2021-12-02 09:02:44 +0200

    day 2 solution

--- solutions/day01/solution.py
+++ solutions/day01/solution.py
@@ -7,9 +7,47 @@ with open("input.txt", "r") as f:
     input_data = f.read()
 
 # Write your solution here
-def solution(input_data):
-    pass
+def solution_part_1(input_data):
+    commands = input_data.split("\n")
+
+    horizontal = 0
+    depth = 0
+
+    for command in commands:
+        c = command.split(" ")
+        c[1] = int(c[1])
+
+        if c[0] == "forward":
+            horizontal += c[1]
+        elif c[0] == "down":
+            depth += c[1]
+        elif c[0] == "up":
+            depth -= c[1]
+
+    print("Part 1:", horizontal * depth)
+
+def solution_part_2(input_data):
+    commands = input_data.split("\n")
+
+    horizontal = 0
+    depth = 0
+    aim = 0
+
+    for command in commands:
+        c = command.split(" ")
+        c[1] = int(c[1])
+
+        if c[0] == "forward":
+            horizontal += c[1]
+            depth += (aim * c[1])
+        elif c[0] == "down":
+            aim += c[1]
+        elif c[0] == "up":
+            aim -= c[1]
+
+    print("Part 2:", horizontal * depth)
 
 
 if __name__ == "__main__":
-    solution(input_data)
+    solution_part_1(input_data)
+    solution_part_2(input_data)
