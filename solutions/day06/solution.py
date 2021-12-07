5b08503c78a1e81cdc38ce35b90e4a8b4b9250f0
Alexander Stoichkov <sashostoichkov@gmail.com>
2021-12-07 22:25:37 +0200

    day 7 solution

--- solutions/day06/solution.py
+++ solutions/day06/solution.py
@@ -5,11 +5,31 @@ import os
 # Read the input file
 with open("input.txt", "r") as f:
     input_data = f.read()
+    input_data = input_data.split(",")
+    input_data = [int(x) for x in input_data]
 
 # Write your solution here
-def solution(input_data):
-    pass
+def get_fuel(number, part):
+    if part == 1:
+        return number
+    else:
+        return (number * (number + 1)) // 2
+
+
+def solution(input_data, part):
+    fuel_array = []
+
+    for i in range(max(input_data)):
+        fuel_sum = 0
+
+        for p in input_data:
+            fuel_sum += get_fuel(abs(p - i), part)
+
+        fuel_array.append(fuel_sum)
+
+    print(f"Part {part}: {min(fuel_array)}")
 
 
 if __name__ == "__main__":
-    solution(input_data)
+    solution(input_data, 1)
+    solution(input_data, 2)
